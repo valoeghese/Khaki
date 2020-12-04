384661ff3155e32a5b10cc894eb52e3b316cdc35
valoeghese <valoeghesemods@gmail.com>
2020-12-05 09:34:47 +1300

    Initial Commit.

---
+++ src/main/java/valoeghese/khaki/Khaki.java
@@ -0,0 +1,17 @@
+package valoeghese.khaki;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Khaki implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Khaki");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
