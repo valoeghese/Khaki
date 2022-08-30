c3b3a6daafc2a80053a90d193db655d1ffb1ff1a
Valoeghese <valoeghesemods@gmail.com>
2022-08-30 18:58:48 +1200

    steal some code from jwg another project i work on but remove the fastInvDist?? why???

---
+++ src/main/java/valoeghese/strom/Maths.java
@@ -0,0 +1,26 @@
+package valoeghese.strom;
+
+public final class Maths {
+	private Maths() {}
+
+	public static double map(double value, double min, double max, double newmin, double newmax) {
+		double prog = (value - min) / (max - min);
+		return newmin + prog * (newmax - newmin);
+	}
+
+	public static double clampMap(double value, double min, double max, double newmin, double newmax) {
+		double prog = (value - min) / (max - min);
+		if (prog <= 0) return newmin;
+		if (prog >= 1) return newmax;
+		return newmin + prog * (newmax - newmin);
+	}
+
+	public static int floor(double d) {
+		int i = (int) d;
+		return d < i ? i - 1 : i;
+	}
+
+	public static double sqr(double d) {
+		return d * d;
+	}
+}
